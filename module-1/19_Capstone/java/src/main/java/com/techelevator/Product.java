884cd49da70ad52a8723b6f9af83b174129bd6b4
Dana Liss <danaliss@gmail.com>
2019-10-10 14:59:19 -0400

    set up product abstract class

--- module-1/19_Capstone/java/src/main/java/com/techelevator/Product.java
+++ module-1/19_Capstone/java/src/main/java/com/techelevator/Product.java
@@ -1,68 +1,37 @@
 package com.techelevator;
 
-public class Product {
-	private String slotID;
-	private String productName;
-	private double price;
-	private String productType;
-	private int quantity;
+import org.w3c.dom.NameList;
 
+public abstract class Product {
+	private String name;
+	private String type;
+	private int price;
+	private String sound;
 
-	public Product(String slot, String name, double price, String productType, int quantity) {
-		this.slotID = slot;
-		this.productName = name;
+	
+	public Product(String name, String type, int price, String sound) {
+		this.name = name;
+		this.type = type;
 		this.price = price;
-		this.quantity = quantity;
-		this.productType = productType;
+		this.sound = sound;
 	}
 
 	
-
-	public String getProductType() {
-		return productType;
-	}
-
-
-
-	public String getSlot() {
-		return slotID;
+	public String getType() {
+		return type;
 	}
 
 	public String getName() {
-		return productName;
+		return name;
 	}
 
-	public double getPrice() {
+	public int getPrice() {
 		return price;
 	}
 
-	public int getQuantity() {
-		return quantity;
+	public int getSound() {
+		return price;
 	}
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-
-	}
-
-	public void decreaseQuantity() {
-		quantity--;
-
-	}
-
-	public String isSoldOut() {
-		if (quantity == 0) {
-			return "This product is sold out. Please make another selection";
-		} else {
-			return Integer.toString(quantity);
-		}
-	}
-
-	@Override
-	public String toString() {
-		String convertToCharacters = String.format("%1$-5s %2$-25s %3$-10s %4$-20s", slotID, productName, price, isSoldOut());
-
-		return convertToCharacters;
-	}
 
 }
